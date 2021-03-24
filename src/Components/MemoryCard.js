1e134b2c295fd0074524066241ce988549f07ad0
Brandon <woolstonbj11@gmail.com>
2021-03-24 12:11:07 -0400

    adding the flipped state to memorycard component

--- src/Components/MemoryCard.js
+++ src/Components/MemoryCard.js
@@ -2,10 +2,34 @@ import React from 'react';
 import './MemoryCard.css'
 
 class MemoryCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFlipped: false
+        };
+    }
+
+    clickHandler = () => {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        });
+    }
+
     render() {
+        let innerClass = "MemoryCard__inner "
+        if (this.state.isFlipped === true) {
+            innerClass = innerClass.concat("flipped")
+        }
         return(
-            <div className="memoryCard">
-                <img className="DClogo"src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="digitalcrafts logo"></img>
+            <div className="MemoryCard" onClick={this.clickHandler}>
+                <div className={innerClass}>
+                    <div className="MemoryCard__back">
+                        <img className="DClogo"src="https://www.digitalcrafts.com/img/logo-wrench-white.png" alt="digitalcrafts logo"></img>
+                    </div>
+                    <div className="MemoryCard__front">
+                            ∆
+                    </div>
+                </div>
             </div>
         )
     }
